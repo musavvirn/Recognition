ed36bb104a3320261a14a9042863463d0cf71fb2
musavvir.noor <musavvir.noor@gmail.com>
2020-05-05 00:46:37 -0400

    detector separated from camera feed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,8 +5,10 @@ import javafx.scene.image.Image;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
 import org.opencv.videoio.VideoCapture;
 
 import java.awt.*;
@@ -15,6 +17,12 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class Controller {
+    private final String HAAR_PATH = "D:/Coding/GitHub/Recognition/src/resources/haarcascades/haarcascade_frontalface_alt.xml";
+    private final String LBP_PATH = "D:/Coding/GitHub/Recognition/src/resources/lbpcascades/lbpcascade_frontalface.xml";
+
+    private CascadeClassifier faceCascade = new CascadeClassifier();
+    private int absoluteFaceSize = 0;
+    private boolean detectorON = false;
 
     @FXML
     private Button button;
@@ -27,7 +35,9 @@ public class Controller {
 
     @FXML
     protected void detectObj() {
-
+        this.detectorON = true;
+        this.loadCascade();
+        this.grabframe();
     }
     @FXML
     protected void markObj() {
@@ -47,7 +57,8 @@ public class Controller {
 
             if (this.capture.isOpened()) {
                 this.cameraActive = true;
-
+                // load Haar or LBP cascade classifer
+                //this.loadCascade();
                 Runnable frameGrabber = new Runnable() {
                     @Override
                     public void run() {
@@ -60,7 +71,7 @@ public class Controller {
 
                 this.timer = Executors.newSingleThreadScheduledExecutor();
                 this.timer.scheduleAtFixedRate(frameGrabber, 0, 33, TimeUnit.MILLISECONDS);
-                this.button.setLabel("Stop Camera");
+                //this.button.setLabel("Stop Camera");
 
             } else {
                 System.err.println("Impossible to open camera...");
@@ -74,6 +85,48 @@ public class Controller {
 
     }
 
+    private void detectAndDisplay(Mat frame)
+    {
+        MatOfRect faces = new MatOfRect();
+        Mat grayFrame = new Mat();
+
+        // convert the frame in gray scale
+        Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
+        // equalize the frame histogram to improve the result
+        Imgproc.equalizeHist(grayFrame, grayFrame);
+
+        // compute minimum face size (20% of the frame height, in our case)
+        if (this.absoluteFaceSize == 0)
+        {
+            int height = grayFrame.rows();
+            if (Math.round(height * 0.2f) > 0)
+            {
+                this.absoluteFaceSize = Math.round(height * 0.2f);
+            }
+        }
+
+        // detect faces
+        this.faceCascade.detectMultiScale(grayFrame, faces, 1.1, 2, 0 | Objdetect.CASCADE_SCALE_IMAGE,
+                new Size(this.absoluteFaceSize, this.absoluteFaceSize), new Size());
+
+        // each rectangle in faces is a face: draw them!
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++)
+            Imgproc.rectangle(frame, facesArray[i].tl(), facesArray[i].br(), new Scalar(0, 255, 0), 3);
+
+    }
+
+    /* Set cascade classifier path to either Haar or LBP in /resources */
+
+    private void loadCascade()
+    {
+        // load the classifier(s)
+        this.faceCascade.load(HAAR_PATH);
+
+        // now the video capture can start
+        //this.cameraButton.setDisable(false);
+    }
+
     private Mat grabframe() {
         Mat frame = new Mat();
 
@@ -81,6 +134,10 @@ public class Controller {
             try {
                 this.capture.read(frame);
                 if (!frame.empty()) {
+                    if (this.detectorON) {
+                        this.detectAndDisplay(frame);
+
+                    }
                     Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BayerRG2GRAY);
 
                 }
@@ -94,6 +151,7 @@ public class Controller {
     }
 
     private void updateImageView(ImageView view, Image image) {
+
         Utils.onFXThread(view.imageProperty(), image);
     }
 
